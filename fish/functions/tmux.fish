215e1837a0fc52dc0402466aae610411d0adea75
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-04-16 23:19:24 +0200

    now running updates only when using tmux without arguments

--- fish/functions/tmux.fish
+++ fish/functions/tmux.fish
@@ -1,26 +1,28 @@
 function tmux
-  if type -q omf
-    # Update Oh My Fish if it installed
-    omf update
-  end
+  if test (count $argv) -eq 0
+    if type -q omf
+      # Update Oh My Fish if it installed
+      omf update
+    end
 
-  if test ! -d ~/.tmux/plugins/tpm
-    # Install TPM if it is not already installed
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    if test ! -d ~/.tmux/plugins/tpm
+      # Install TPM if it is not already installed
+      git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-    # Install TPM plugins
-    ~/.tmux/plugins/tpm/bin/install_plugins
-  else
-    # Install plugins that are not already installed
-    ~/.tmux/plugins/tpm/bin/install_plugins
+      # Install TPM plugins
+      ~/.tmux/plugins/tpm/bin/install_plugins
+    else
+      # Install plugins that are not already installed
+      ~/.tmux/plugins/tpm/bin/install_plugins
 
-    # Update plugins that needs update
-    ~/.tmux/plugins/tpm/bin/update_plugins all
+      # Update plugins that needs update
+      ~/.tmux/plugins/tpm/bin/update_plugins all
 
-    # Clean plugins that are not used anymore
-    ~/.tmux/plugins/tpm/bin/update_plugins all
+      # Clean plugins that are not used anymore
+      ~/.tmux/plugins/tpm/bin/update_plugins all
+    end
   end
 
   # Execute TMUX
-  /usr/bin/tmux
+  /usr/bin/tmux $argv
 end
