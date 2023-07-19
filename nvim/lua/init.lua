e935dcbda6d06ba629bf45fba584b87917ae1bd1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-07-20 00:07:20 +0200

    various imporovement over the shortcuts for telescope

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -4,6 +4,9 @@ vim.o.tabstop = 2
 vim.o.number = true
 vim.o.relativenumber = true
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
@@ -44,16 +47,16 @@ require("lazy").setup({
 	},
 	{
 		"nvim-tree/nvim-tree.lua",
-		lazy = false,
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
+		},
+		keys = {
+			{ "<c-n>", "<cmd>NvimTreeOpen<cr>", silent = true, desc = "Open the file tree" }
 		},
 		config = function() 
 			vim.g.loaded_netrw = 1
 			vim.g.loaded_netrwPlugin = 1
 			vim.opt.termguicolors = true
-
-			vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeOpen<CR>", { silent = true })
 
 			require("nvim-tree").setup({
 				auto_reload_on_write = true,
@@ -65,6 +68,28 @@ require("lazy").setup({
 					dotfiles = false
 				}
 			})
+		end
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim"
+		},
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files (without hidden nor ignored files)" },
+			{ "<leader>fw", "<cmd>Telescope live_grep<CR>", desc = "Find words" },
+			{ "<leader>fc", "<cmd>Telescope find_colorschemes<CR>", desc = "Find color schemes" },
+		},
+		config = function()
+			require("telescope").setup({})
+		end
+	},
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 0
+			require("which-key").setup({})
 		end
 	}
 })
