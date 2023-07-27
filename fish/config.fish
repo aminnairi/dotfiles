e39a8c136a51047ded4190326fbbcae2c9382044
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-07-27 11:07:35 +0200

    revert back to a darker theme

--- fish/config.fish
+++ fish/config.fish
@@ -8,9 +8,6 @@ if status is-interactive
   # Set the editor to use vim
   set --export --global EDITOR nvim
 
-  # Set the terminal
-  set --export --global TERM "screen-256color"
-
   # Install OMF if it is not already
   if test ! -d ~/.local/share/omf
     curl -L https://get.oh-my.fish | fish
