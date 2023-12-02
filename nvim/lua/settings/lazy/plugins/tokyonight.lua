966c05f4fb537efab146101b1eec726b09559dda
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-02 23:53:11 +0100

    using lua function to define the colorscheme

--- nvim/lua/settings/lazy/plugins/tokyonight.lua
+++ nvim/lua/settings/lazy/plugins/tokyonight.lua
@@ -1,14 +1,16 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  init = function()
-    vim.cmd([[colorscheme tokyonight]])
-  end,
-  opts = {
-    style = "storm",
-    light_style = "day",
-    transparent = false,
-    terminal_colors = true,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme("tokyonight")
+    end,
+    opts = {
+      style = "storm",
+      transparent = false,
+      light_style = "day",
+      terminal_colors = true,
+    },
   },
 }
