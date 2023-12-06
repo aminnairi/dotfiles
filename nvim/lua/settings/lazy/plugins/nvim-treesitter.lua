4ba859ac10480a4ecad975f559a119251151303a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-06 04:51:27 +0100

    now indenting based on the treesitter AST

--- nvim/lua/settings/lazy/plugins/nvim-treesitter.lua
+++ nvim/lua/settings/lazy/plugins/nvim-treesitter.lua
@@ -43,6 +43,10 @@ return {
       ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
       -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
+      indent = {
+        enable = true
+      },
+
       highlight = {
         enable = true,
 
