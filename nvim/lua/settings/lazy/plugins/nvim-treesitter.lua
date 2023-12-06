7b15cfc56277dd07cf212c079181a6d426d8a979
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-06 04:51:07 +0100

    now automatically installing treesitter languages

--- nvim/lua/settings/lazy/plugins/nvim-treesitter.lua
+++ nvim/lua/settings/lazy/plugins/nvim-treesitter.lua
@@ -38,7 +38,7 @@ return {
 
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-      auto_install = false,
+      auto_install = true,
 
       ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
       -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
