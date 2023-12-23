917ecac9005131d56602c4727e55ef7d8ae438fb
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-23 22:03:51 +0100

    added more vim-like keybinds in which key

--- nvim/lua/settings/lazy/plugins/which-key.lua
+++ nvim/lua/settings/lazy/plugins/which-key.lua
@@ -5,4 +5,32 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 0
   end,
+  config = function()
+    require("which-key").register({
+      ["<C-s>"] = {
+        ":write",
+        "Persist the content of the current buffer to the file system"
+      },
+      ["<C-q>"] = {
+        ":quitall",
+        "Quit the editor with confirmation"
+      },
+      ["<C-h>"] = {
+        "<C-w>h",
+        "Move to the window on the left"
+      },
+      ["<C-l>"] = {
+        "<C-w>l",
+        "Move to the window on the right"
+      },
+      ["<C-k>"] = {
+        "<C-w>k",
+        "Move to the window on the top"
+      },
+      ["<C-j>"] = {
+        "<C-w>j",
+        "Move to the window on the bottom"
+      }
+    })
+  end
 }
