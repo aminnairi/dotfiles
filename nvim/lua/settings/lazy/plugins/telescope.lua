c1e74d555dd7c16e641f9ee59850770c36f89b39
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-23 22:05:30 +0100

    removed lsp stuff and added some more keybinds

--- nvim/lua/settings/lazy/plugins/telescope.lua
+++ nvim/lua/settings/lazy/plugins/telescope.lua
@@ -4,75 +4,51 @@ return {
     "nvim-lua/plenary.nvim",
   },
   config = function()
-    require("telescope").setup({})
+    local telescope = require("telescope")
+    local telescopeBuiltin = require("telescope.builtin")
+
+    telescope.setup({})
 
     local whichKey = require("which-key")
 
     whichKey.register({
       ["<leader>t"] = {
         name = "Telescope",
+        h = {
+          function()
+            telescopeBuiltin.help_tags()
+          end,
+          "Find help tags"
+        },
         t = {
           function()
-            require("telescope.builtin").treesitter()
+            telescopeBuiltin.treesitter()
           end,
           "Explore the Abstract Syntax Tree",
         },
         f = {
           function()
-            require("telescope.builtin").find_files()
+            telescopeBuiltin.find_files()
           end,
           "Find files",
         },
         w = {
           function()
-            require("telescope.builtin").live_grep()
+            telescopeBuiltin.live_grep()
           end,
           "Find words",
         },
         c = {
           function()
-            require("telescope.builtin").colorscheme()
+            telescopeBuiltin.colorscheme()
           end,
           "Find color schemes",
         },
         g = {
           function()
-            require("telescope.builtin").git_files()
+            telescopeBuiltin.git_files()
           end,
           "Find git files",
-        },
-        l = {
-          name = "LSP",
-          e = {
-            function()
-              require("telescope.builtin").diagnostics()
-            end,
-            "List diagnostics for all open buffers",
-          },
-          r = {
-            function()
-              require("telescope.builtin").lsp_references()
-            end,
-            "List all references for the word under the cursor",
-          },
-          i = {
-            function()
-              require("telescope.builtin").lsp_implementation()
-            end,
-            "Go to the implementation of the word under the cursor",
-          },
-          d = {
-            function()
-              require("telescope.builtin").lsp_definition()
-            end,
-            "Go to the definition of the word under the cursor",
-          },
-          t = {
-            function()
-              require("telescope.builtin").lsp_type_definition()
-            end,
-            "Go to the type definition of the word under the cursor",
-          },
         },
       },
     })
