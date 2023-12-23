ab5bfb4723805b243a812a5811cb2ac63e11a28b
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-23 22:01:43 +0100

    added more keymaps for notifications stuff

--- nvim/lua/settings/lazy/plugins/nvim-notify.lua
+++ nvim/lua/settings/lazy/plugins/nvim-notify.lua
@@ -2,9 +2,39 @@ return {
   "rcarriga/nvim-notify",
   event = "VeryLazy",
   config = function()
-    vim.notify = require("notify")
+    local notify = require("notify")
+
+    vim.notify = notify
+
+    require("which-key").register({
+      ["<leader>u"] = {
+        name = "User Interface",
+        n = {
+          name = "Notification",
+          e = {
+            function()
+              vim.notify = notify
+            end,
+            "Enable notifications"
+          },
+          d = {
+            function()
+              vim.notify = function() end
+            end,
+            "Disable notifications"
+          },
+          l = {
+            function()
+              require("telescope").extensions.notify.notify()
+            end,
+            "List notifications"
+          }
+        }
+      }
+    })
 
     require("notify.config").setup({
+      enabled = false,
       stages = "fade_in_slide_out",
       timeout = 5000,
       background_colour = "#1e222a",
