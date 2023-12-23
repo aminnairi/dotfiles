93ba49bac6324320a20fef7f45d68f07fcdba072
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-23 22:02:29 +0100

    updated keymaps to a more vim-like feel

--- nvim/lua/settings/lazy/plugins/nvimcmp.lua
+++ nvim/lua/settings/lazy/plugins/nvimcmp.lua
@@ -23,11 +23,13 @@ return {
         documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-l>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
         { name = "copilot", group_index = 2 },
