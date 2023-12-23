e5a2051873e4a7407ad95df29540a0b4e4e2c7a0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-23 22:00:53 +0100

    moved keymaps to which key

--- nvim/lua/settings/keymap.lua
+++ nvim/lua/settings/keymap.lua
@@ -1,8 +1,2 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-q>", ":wqa!<cr>", { silent = true })
