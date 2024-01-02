009988fef39777485bf60eed8d1567bffb1aac46
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-01-02 22:59:20 +0100

    removed unecessary functions

--- ansible/roles/fish/files/fish/functions/increase_brightness_by.fish
+++
@@ -1,5 +0,0 @@
-function increase_brightness_by --argument-names amount
-  set --local brightness (cat /sys/class/backlight/*/brightness)
-  set --local brightness_increased (math $brightness + $amount)
-  echo $brightness_increased | sudo tee /sys/class/backlight/*/brightness > /dev/null
-end
--- ansible/roles/fish/files/fish/functions/set_brightness.fish
+++
@@ -1,3 +0,0 @@
-function set_brightness --argument-names amount
-  echo $amount | sudo tee /sys/class/backlight/*/brightness > /dev/null
-end
