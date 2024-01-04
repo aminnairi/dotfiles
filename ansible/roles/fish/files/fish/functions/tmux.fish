7446c5b7805c62c41197fed42d549c6048830d01
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-01-04 11:35:10 +0100

    tmux does not search for oh my fish updates at startup anymore

--- ansible/roles/fish/files/fish/functions/tmux.fish
+++ ansible/roles/fish/files/fish/functions/tmux.fish
@@ -1,10 +1,5 @@
 function tmux
   if test (count $argv) -eq 0
-    if type -q omf
-      # Update Oh My Fish if it installed
-      omf update
-    end
-
     # Install Tmux plugins
     ~/.tmux/plugins/tpm/bin/install_plugins
 
