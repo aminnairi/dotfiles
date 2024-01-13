be5e7a6d24c02530017721ce9516c207b4019c0f
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-01-13 20:00:41 +0100

    added more settings for neorg

--- ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/neorg.lua
+++ ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/neorg.lua
@@ -1,42 +1,77 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  init = function()
-    vim.g.maplocalleader = ","
-  end,
-  config = function()
-    require("neorg").setup {
-      load = {
-        ["core.defaults"] = {},
-        ["core.concealer"] = {
-          config = {
-            -- Configure icons for concealed items
-            icons = {
-              -- Configure the headings
-              heading = {
-                -- List of icons to use for all headings levels from 1 to 6
-                icons = {
-                  "◉",
-                  "◎",
-                  "○",
-                  "◉",
-                  "◎",
-                  "○"
-                }
-              }
-            }
-          }
-        },
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              notes = "~/git/github.com/aminnairi/notes",
-            },
-          },
-        },
-      },
-    }
-    -- TODO: add keybinds for neorg
-  end,
+	"nvim-neorg/neorg",
+	build = ":Neorg sync-parsers",
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"folke/which-key",
+	},
+	init = function()
+		-- Configure the key to use for keybinds used in the "core.keybinds" module
+		vim.g.maplocalleader = ","
+	end,
+	config = function()
+		require("neorg").setup({
+			load = {
+				-- Default module that imports the core modules necessary for Neorg
+				["core.defaults"] = {},
+				-- Module responsible for enhancing the icons display in Neorg files
+				["core.concealer"] = {
+					-- Configuration for the "core.concealer" module
+					config = {
+						-- Configure icons for concealed items
+						icons = {
+							-- Configure the headings
+							heading = {
+								-- List of icons to use for all headings levels from 1 to 6
+								icons = {
+									"◉",
+									"◎",
+									"○",
+									"◉",
+									"◎",
+									"○",
+								},
+							},
+						},
+					},
+				},
+				["core.dirman"] = {
+					-- Configuration for "core.dirman" module
+					config = {
+						-- Workspaces are folders from where to find Neorg files
+						workspaces = {
+							-- My personal notes
+							notes = "~/git/github.com/aminnairi/notes",
+						},
+					},
+				},
+				-- Module for controling the presentation mode for Neorg files
+				["core.presenter"] = {
+					-- Configuration for the presenter module
+					config = {
+						-- Plugin to use, here "folke/zen-mode"
+						zen_mode = "zen-mode",
+					},
+				},
+			},
+		})
+
+		require("which-key").register({
+			["<leader>o"] = {
+				name = "Neorg",
+				p = {
+					name = "Presenter",
+					s = {
+						":Neorg presenter start",
+						"Start",
+					},
+					S = {
+						":Neorg presenter stop",
+						"Stop",
+					},
+				},
+			},
+		})
+	end,
 }
