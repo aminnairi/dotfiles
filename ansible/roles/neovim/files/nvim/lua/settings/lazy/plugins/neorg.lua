949ddd2389b5b5d0d7d53790fab988e39eebc5f1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-01-13 11:18:10 +0100

    added neorg configuration file

---
+++ ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/neorg.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup {
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/git/github.com/aminnairi/notes",
+            },
+          },
+        },
+      },
+    }
+  end,
+}
