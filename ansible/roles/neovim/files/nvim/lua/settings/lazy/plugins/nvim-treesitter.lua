c7f4d3e26f75c0d172076fce925f7159ea247eac
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-03-08 12:26:20 +0100

    attempt at fixing vue syntax

--- ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/nvim-treesitter.lua
+++ ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/nvim-treesitter.lua
@@ -43,7 +43,7 @@ return {
       },
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+      sync_install = true,
 
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
@@ -57,7 +57,7 @@ return {
       },
 
       highlight = {
-        enable = true,
+        enable = false,
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
