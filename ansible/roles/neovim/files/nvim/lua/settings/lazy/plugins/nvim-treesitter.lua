e0b0fe653aa90365fc0349b3646c43ea9152e479
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-03-09 20:10:39 +0100

    revert back to using syntax highlight

--- ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/nvim-treesitter.lua
+++ ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/nvim-treesitter.lua
@@ -47,7 +47,7 @@ return {
 
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-      auto_install = false,
+      auto_install = true,
 
       ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
       -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
@@ -57,7 +57,7 @@ return {
       },
 
       highlight = {
-        enable = false,
+        enable = true,
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
