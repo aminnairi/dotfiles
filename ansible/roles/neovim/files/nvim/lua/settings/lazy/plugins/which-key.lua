0e9cdbb8ef45af11baf8cd3a789ceb84340fabce
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-05 10:23:55 +0100

    using the new format for which-key for window mappings

--- ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/which-key.lua
+++ ansible/roles/neovim/files/nvim/lua/settings/lazy/plugins/which-key.lua
@@ -7,12 +7,12 @@ return {
   end,
   config = function()
     require("which-key").register({
-      { "<C-h>", "<C-w>h", desc = "Move to the window on the left" },
-      { "<C-j>", "<C-w>j", desc = "Move to the window on the bottom" },
-      { "<C-k>", "<C-w>k", desc = "Move to the window on the top" },
-      { "<C-l>", "<C-w>l", desc = "Move to the window on the right" },
-      { "<C-q>", ":quitall<CR>", desc = "Quit the editor with confirmation" },
-      { "<C-s>", ":write<CR>", desc = "Persist the content of the current buffer to the file system" },
+      { "", "<C-l>", desc = "<C-w>l" },
+      { "", "<C-h>", desc = "<C-w>h" },
+      { "", "<C-j>", desc = "<C-w>j" },
+      { "", "<C-s>", desc = ":write<CR>" },
+      { "", "<C-q>", desc = ":quitall<CR>" },
+      { "", "<C-k>", desc = "<C-w>k" },
     })
   end
 }
