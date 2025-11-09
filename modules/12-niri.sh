4c72d12f1472ce4916f9f6c4603588f60d5e9111
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-09 10:35:30 +0100

    added more optional dependencies for noctalia

--- modules/12-niri.sh
+++ modules/12-niri.sh
@@ -13,5 +13,12 @@ ensure_package_is_installed niri \
   && ensure_package_is_installed gnome-keyring \
   && ensure_package_is_installed plasma-polkit-agent \
   && ensure_package_is_installed xwayland-satellite \
+  && ensure_package_is_installed cliphist \
+  && ensure_package_is_installed matugen-git \
+  && ensure_package_is_installed cava \
+  && ensure_package_is_installed wlsunset \
+  && ensure_package_is_installed xdg-desktop-portal \
+  && ensure_package_is_installed python3 \
+  && ensure_package_is_installed evolution-data-server \
   && synchronize_user_folder ./sources/niri ~/.config \
   && synchronize_root_folder ./sources/niri ~/.config
