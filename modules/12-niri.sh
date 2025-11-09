495d55688eb886f9b54a1de87f99b4d8f8b7b4b8
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-09 10:21:37 +0100

    initial commit

---
+++ modules/12-niri.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ensure_package_is_installed niri \
+  && ensure_package_is_installed gdm \
+  && ensure_package_is_installed quickshell \
+  && ensure_package_is_installed ttf-roboto \
+  && ensure_package_is_installed inter-font \
+  && ensure_package_is_installed gpu-screen-recorder \
+  && ensure_package_is_installed brightnessctl \
+  && ensure_package_is_installed noctalia-shell \
+  && ensure_package_is_installed xdg-desktop-portal-gtk \
+  && ensure_package_is_installed xdg-desktop-portal-gnome \
+  && ensure_package_is_installed gnome-keyring \
+  && ensure_package_is_installed plasma-polkit-agent \
+  && ensure_package_is_installed xwayland-satellite \
+  && synchronize_user_folder ./sources/niri ~/.config \
+  && synchronize_root_folder ./sources/niri ~/.config
