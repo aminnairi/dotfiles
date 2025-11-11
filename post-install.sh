fc9b2f1dcf83fe57881b11ba0dff8b44f563d206
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-11 13:15:11 +0100

    removed niri install script in the post install script

--- post-install.sh
+++ post-install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-source ./modules/00-library.sh \
-  && source ./modules/01-git.sh \
-  && source ./modules/02-base.sh \
-  && source ./modules/03-docker.sh \
-  && source ./modules/04-neovim.sh \
-  && source ./modules/05-fish.sh \
-  && source ./modules/06-fontconfig.sh \
-  && source ./modules/07-services.sh \
-  && source ./modules/08-gnome.sh \
-  && source ./modules/09-kitty.sh \
-  && source ./modules/10-tmux.sh \
-  && source ./modules/11-syncthing.sh \
-  && source ./modules/12-niri.sh
+source ./modules/00-library.sh &&
+  source ./modules/01-git.sh &&
+  source ./modules/02-base.sh &&
+  source ./modules/03-docker.sh &&
+  source ./modules/04-neovim.sh &&
+  source ./modules/05-fish.sh &&
+  source ./modules/06-fontconfig.sh &&
+  source ./modules/07-services.sh &&
+  source ./modules/08-gnome.sh &&
+  source ./modules/09-kitty.sh &&
+  source ./modules/10-tmux.sh &&
+  source ./modules/11-syncthing.sh &&
+  source ./modules/12-graphics.sh
