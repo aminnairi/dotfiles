14ecb73aa5c6e77ff28e719e4cdfcd5b292bcb7a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-11 14:11:02 +0100

    added operator mono with ligatures

--- modules/09-kitty.sh
+++ modules/09-kitty.sh
@@ -3,6 +3,6 @@
 ensure_package_is_installed kitty &&
   ensure_package_is_installed viu &&
   ensure_package_is_installed ueberzugpp &&
+  ensure_package_is_installed otf-operator-lig-mono &&
   synchronize_user_folder ./sources/kitty ~/.config &&
   synchronize_root_folder ./sources/kitty /root/.config
-
