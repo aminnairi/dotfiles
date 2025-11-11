e6fba0d13abf68de89c127060b0b0a093e261ceb
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-11 16:17:39 +0100

    fixed path for synchronizing the tmux configuration

--- modules/10-tmux.sh
+++ modules/10-tmux.sh
@@ -3,6 +3,5 @@
 ensure_package_is_installed tmux &&
   ensure_git_folder_installed_for_user tmux-plugins/tpm ~/.tmux/plugins/tpm &&
   ensure_git_folder_installed_for_root tmux-plugins/tpm /root/.tmux/plugins/tpm &&
-  synchronize_user_folder ./sources/tmux/.tmux.conf ~/.config &&
-  synchronize_root_folder ./sources/tmux/.tmux.conf /root/.config
-
+  synchronize_user_folder ./sources/tmux/.tmux.conf ~/ &&
+  synchronize_root_folder ./sources/tmux/.tmux.conf /root
