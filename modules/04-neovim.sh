1ad910896a5624048a9cbb514bbd88bccbc83243
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-11 13:14:29 +0100

    ensuring lazygit is installing along with neovim

--- modules/04-neovim.sh
+++ modules/04-neovim.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-ensure_package_is_installed nvim \
-  && synchronize_user_folder ./sources/nvim/ ~/.config/nvim
+ensure_package_is_installed nvim &&
+  ensure_package_is_installed lazygit &&
+  synchronize_user_folder ./sources/nvim/ ~/.config/nvim &&
+  synchronize_root_folder ./sources/nvim /root/.config/nvim
+
