3bed3b3db36aab76a7ba7fe321eb6e81ce4ac9e2
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-11 14:11:20 +0100

    initial commit

---
+++ modules/12-yazi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ensure_package_is_installed yazi &&
+  ensure_package_is_installed ffmpeg &&
+  ensure_package_is_installed 7zip &&
+  ensure_package_is_installed jq &&
+  ensure_package_is_installed poppler &&
+  ensure_package_is_installed fd &&
+  ensure_package_is_installed rg &&
+  ensure_package_is_installed fzf &&
+  ensure_package_is_installed zoxide &&
+  ensure_package_is_installed resvg &&
+  ensure_package_is_installed imagemagick &&
+  ensure_package_is_installed wl-clipboard
