d929fd881b00254cf69e7888f5444bf3c0812bc1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-12-17 18:58:57 +0100

    ensuring bluetooth related packages are installed

--- modules/14-steam.sh
+++ modules/14-steam.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-ensure_package_is_installed steam
+ensure_package_is_installed steam &&
+  ensure_package_is_installed bluez &&
+  ensure_package_is_installed bluez-utils &&
+  ensure_package_is_installed xone-dkms
