1d29cb6cb5e404018d93aaef945c19ae953020d0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-12-19 09:31:22 +0100

    finally fixed bluetooth issue affecting low energy into classic mode devices such as the xbox controller

--- modules/14-steam.sh
+++ modules/14-steam.sh
@@ -3,4 +3,5 @@
 ensure_package_is_installed steam &&
   ensure_package_is_installed bluez &&
   ensure_package_is_installed bluez-utils &&
-  ensure_package_is_installed xone-dkms
+  ensure_package_is_installed xone-dkms &&
+  sed -i 's/ControllerMode = bredr/ControllerMode = dual/g' /etc/bluetooth/main.conf
