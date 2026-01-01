1baec4fc76dcec9b2b0c2f351bea0fadf47bbcbd
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-01 11:37:24 +0100

    removed bluetooth file configuration

--- modules/14-steam.sh
+++ modules/14-steam.sh
@@ -3,5 +3,4 @@
 ensure_package_is_installed steam &&
   ensure_package_is_installed bluez &&
   ensure_package_is_installed bluez-utils &&
-  ensure_package_is_installed xone-dkms &&
-  sed -i 's/ControllerMode = bredr/ControllerMode = dual/g' /etc/bluetooth/main.conf
+  ensure_package_is_installed xone-dkms
