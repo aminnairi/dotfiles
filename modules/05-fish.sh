bae7ed62ee62e43a32745e074ee721665c8bb80c
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-04 20:56:08 +0100

    now removing entirely the fish folder in order to prevent conflict with older files

--- modules/05-fish.sh
+++ modules/05-fish.sh
@@ -6,13 +6,16 @@ ensure_package_is_installed fish &&
   ensure_package_is_installed fastfetch &&
   ensure_package_is_installed openvpn &&
   ensure_package_is_installed fzf &&
-  synchronize_user_folder ./sources/fish ~/.config &&
-  synchronize_user_folder ./sources/omf ~/.config &&
-  synchronize_root_folder ./sources/fish /root/.config &&
-  synchronize_root_folder ./sources/omf /root/.config &&
+  rm -rf ~/.config/fish/themes &&
+  ensure_user_folder_present ~/.config/fish &&
+  synchronize_user_folder ./sources/fish/ ~/.config/fish &&
+  ensure_user_folder_present ~/.config/omf &&
+  synchronize_user_folder ./sources/omf/ ~/.config/omf &&
+  sudo rm -rf /root/.config/fish/themes &&
+  synchronize_root_folder ./sources/fish/ /root/.config/fish &&
+  synchronize_root_folder ./sources/omf/ /root/.config/omf &&
   ensure_git_folder_installed_for_user oh-my-fish/oh-my-fish ~/.local/share/omf &&
   ensure_git_folder_installed_for_user oh-my-fish/theme-agnoster ~/.local/share/omf/themes/agnoster &&
   ensure_git_folder_installed_for_root oh-my-fish/oh-my-fish /root/.local/share/omf &&
   ensure_git_folder_installed_for_root oh-my-fish/theme-agnoster /root/.local/share/omf/themes/agnoster &&
   ensure_shell_installed fish
-
