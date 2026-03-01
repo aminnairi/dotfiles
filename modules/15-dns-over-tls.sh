ddd404376e9c850c20c0786d5f0436324f325945
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-01 10:24:59 +0100

    removed dns sec and domain setting for dot

--- modules/15-dns-over-tls.sh
+++ modules/15-dns-over-tls.sh
@@ -1,5 +1,3 @@
 sudo sed -i "s/^#\?DNS=.*/DNS=157.90.170.95#dns.nairi.cloud/" /etc/systemd/resolved.conf &&
   sudo sed -i "s/^#\?DNSOverTLS=.*/DNSOverTLS=yes/" /etc/systemd/resolved.conf &&
-  sudo sed -i "s/^#\?DNSSEC=.*/DNSSEC=yes/" /etc/systemd/resolved.conf &&
-  sudo sed -i "s/^#\?Domains=.*/Domains=~./" /etc/systemd/resolved.conf &&
   ensure_service_active systemd-resolved
