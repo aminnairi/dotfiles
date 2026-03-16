f3f6d320b72127545cf8e6cde031a8f28bd96e42
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-16 06:06:01 +0100

    added opencode module in the post-install script

--- post-install.sh
+++ post-install.sh
@@ -16,4 +16,5 @@ source ./modules/00-library.sh &&
   source ./modules/12-yazi.sh &&
   source ./modules/13-rust.sh &&
   source ./modules/14-steam.sh &&
-  source ./modules/15-dns-over-tls.sh
+  source ./modules/15-dns-over-tls.sh &&
+  source ./modules/16-opencode.sh
