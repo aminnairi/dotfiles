248fb6315964c857d6efce7877e8ec3c6289ba1f
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-18 22:10:52 +0100

    added a command for updating all tmux plugins when running the bash post-install script

--- modules/10-tmux.sh
+++ modules/10-tmux.sh
@@ -5,4 +5,5 @@ source ./modules/00-library.sh &&
   ensure_git_folder_installed_for_user tmux-plugins/tpm ~/.tmux/plugins/tpm &&
   ensure_git_folder_installed_for_root tmux-plugins/tpm /root/.tmux/plugins/tpm &&
   synchronize_user_folder ./sources/tmux/.tmux.conf ~/ &&
-  synchronize_root_folder ./sources/tmux/.tmux.conf /root
+  synchronize_root_folder ./sources/tmux/.tmux.conf /root &&
+  ~/.tmux/plugins/tpm/bin/update_plugins all
