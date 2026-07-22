3355ac2cbf7c4c1629fa69e19d5f4827f644e76e
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-07-22 22:31:53 +0200

    removed dns over tls

--- modules/15-dns-over-tls.sh
+++ modules/15-dns-over-tls.sh
@@ -1,4 +1,4 @@
 source ./modules/00-library.sh &&
   sudo sed -i "s/^#\?DNS=.*/DNS=157.90.170.95#dns.nairi.cloud/" /etc/systemd/resolved.conf &&
-  sudo sed -i "s/^#\?DNSOverTLS=.*/DNSOverTLS=yes/" /etc/systemd/resolved.conf &&
+  sudo sed -i "s/^#\?DNSOverTLS=.*/DNSOverTLS=false/" /etc/systemd/resolved.conf &&
   ensure_service_active systemd-resolved
