03aac349239f134686a225f6ac1f9c9f34315a47
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-07-22 22:32:07 +0200

    removed env loading from fish config

--- sources/fish/config.fish
+++ sources/fish/config.fish
@@ -1,9 +1,4 @@
 if status is-interactive
-    # Load the environment variable (specific to Avante Neovim plugin)
-    if test -f ~/.env
-        source ~/.env
-    end
-
     # Enable vi-mode
     fish_vi_key_bindings
 
